873442c88a8471c594fc4dd12f000ddfa242e0aa
unknown <malami@spbfamily.eu>
2020-03-22 17:24:14 +0100

    README.md & Schema

---
+++ README.mk
@@ -0,0 +1,10 @@
+# Gestion d'environement de développement
+
+## Angular-Symfony
+
+4 Contenaire :
+- MySQL : Pour la base de donnée
+- NGINX et PHP-FPM : Pour Symfony
+- NODE : Pour Angular
+
+![Angular-Symfony Schema](./Angular-Symfony/Angular-Symfony.png)
